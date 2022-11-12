88e990d9f78cc278eb40989ac3fc21aabac623dd
Theo Fandrich <thelogfan42@gmail.com>
2022-11-12 13:49:28 -0800

    preparing deployment

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -29,11 +29,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-router.get("/hello/world", function (req, res) {
-  res.cookie("XSRF-TOKEN", req.csrfToken());
-  res.send("Hello World!");
-});
-
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== "production") {
   router.get("/api/csrf/restore", (req, res) => {
@@ -41,13 +36,5 @@ if (process.env.NODE_ENV !== "production") {
     return res.json({});
   });
 }
-
-// router.get("/api/csrf/restore", (req, res) => {
-//   const csrfToken = req.csrfToken();
-//   res.cookie("XSRF-TOKEN", csrfToken);
-//   res.status(200).json({
-//     "XSRF-Token": csrfToken,
-//   });
-// });
 
 module.exports = router;
